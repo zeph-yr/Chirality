99f5f33a69c8bd4964ef28c7f1aab001231e9a0b
Zephyr <67527746+zeph-yr@users.noreply.github.com>
2022-01-16 17:16:09 +0800

    Update manifest

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chirality(MirrorMod)")]
+[assembly: AssemblyProduct("Chirality")]
 [assembly: AssemblyCopyright("Copyright © Zephyr 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
